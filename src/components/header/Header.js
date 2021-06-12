866b7b4644be8df200b8df6f441236ae44f7f23f
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-06-12 18:40:49 +0300

    Revealed my API key for Heroku deployment.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -23,7 +23,10 @@ function Header() {
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        const API_KEY = "d1540e749ccc1e07651022b415b80efe";
+
+        const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
 
         if (!query) {
             return;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -19,9 +19,11 @@ function Search({
 }) {
     const debouncedValue = useDebounce(query, 200);
 
+    const API_KEY = "d1540e749ccc1e07651022b415b80efe";
+
     // useEffect hook for search input updates.
     useEffect(() => {
-        const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+        const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
 
         async function fetchData(query) {
             if (!query || query.length < 3 || showSuggestions) {
